95d09f2287027848ef135a70de7525f86cf16038
Mikolay789 <mikolay.p789@gmail.com>
2018-12-20 17:48:09 +0100

    menu

---
+++ Assets/Skrypty/buttonlistener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonlistener : MonoBehaviour {
+
+    public string infiniteLevel;
+	
+	public void newgame() {
+
+        SceneManager.LoadScene(infiniteLevel);
+
+    }
+	
+	
+}
